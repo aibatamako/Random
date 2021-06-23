696ed8b81b101fc3457937876bb990343c8dcf2b
Tamako Aiba <t.aiba@4nonome.com>
2021-06-24 06:20:11 +0900

    randomアプリ完成。

--- Random/ViewController.swift
+++ Random/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+      
+        for n in 1...100{
+            let ransu = Int.random(in: 1...100) //1から100の中から１つ選んで変数ransuに入れる
+            print("\(n)個目:\(ransu)")
+        }
     }
 
 
