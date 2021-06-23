0b0ce2704bad78f587446b4ca5add20dba8cf456
Tamako Aiba <t.aiba@4nonome.com>
2021-06-24 06:26:19 +0900

    コメントの追加。

--- Random/ViewController.swift
+++ Random/ViewController.swift
@@ -16,9 +16,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
       
-        for n in 1...100{
+        for n in 1...100{ //n番目かを1から100まで順にnに入れていく
             let ransu = Int.random(in: 1...100) //1から100の中から１つ選んで変数ransuに入れる
             print("\(n)個目:\(ransu)")
+            //デバックエリアに値を表示
         }
     }
 
